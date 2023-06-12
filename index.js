16aeec3afa9edd7c6577b6260f0dff0fada5c43e
jimhome_mac <jsr1611@outlook.com>
2023-06-13 02:00:51 +0900

    header parser project solution done

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+/* return the following
+"ipaddress":"::ffff:159.20.14.100","language":"en-US,en;q=0.5",
+"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+*/
+app.get('/api/whoami', function(req, res){
+  let ip = req.ip;
+  let lang = req.headers['accept-language'];
+  let sw = req.headers['user-agent']
+  res.json({ipaddress: ip,
+          language: lang,
+          software: sw
+  });
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
